107a6e7aa3f9d76203ba4a0364a3a6e466481a5c
Trishant Pahwa <trishantpahwa@gmail.com>
2023-12-03 18:02:06 +0530

    Made package.

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-function awsSES(error) {
-    
-    // Import AWS SD
-    const AWS = require("aws-sdk");
-    // Configure AWS with your access and secret key
-    // (Assuming you have already configured your AWS credentials)
-    AWS.config.update({
-      region: "ap-south-1",
-      accessKeyId: process.env.accessKeyId,
-      secretAccessKey: process.env.secretAccess,
-    });
-  
-    // Create an AWS SES service object
-    const ses = new AWS.SES({ apiVersion: "2010-12-01" });
-  
-    const sendEmail = async () => {
-      const params = {
-        Source: process.env.verifiedEmail, // Verified email
-        Destination: {
-          ToAddresses: [
-            process.env.emails,
-          ],
-        },
-        Message: {
-          Subject: { Data: `Error at  :- , ${process.env.appName}` },
-          Body: {
-            Text: { Data: `Hello, this is a test email from AWS SES. ERROR : - ${JSON.stringify(error)}`  },
+const AWS = require("aws-sdk");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const awsSES = async (error) => {
+  AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+
+  const ses = new AWS.SES({ apiVersion: "2010-12-01" });
+
+  const sendEmail = async () => {
+    const params = {
+      Source: process.env.VERIFIED_EMAIL, // Verified email
+      Destination: {
+        ToAddresses: process.env.EMAILS.split(","),
+      },
+      Message: {
+        Subject: { Data: `Error at  :- , ${process.env.APP_NAME}` },
+        Body: {
+          Text: {
+            Data: `Hello, this is a test email from AWS SES. ERROR : - ${JSON.stringify(
+              error
+            )}`,
           },
         },
-      };
-  
-      try {
-        const response = await ses.sendEmail(params).promise();
-        console.log("Email sent:", response);
-      } catch (error) {
-        console.error("Error sending email:", error);
-      }
+      },
     };
-    sendEmail();
-  }
 
-module.exports.awsSES = awsSES
+    try {
+      await ses.sendEmail(params).promise();
+      return true;
+    } catch (error) {
+      console.error("Error sending email:", error);
+      return false;
+    }
+  };
+  return sendEmail();
+}
+
+module.exports.awsSES = awsSES;
--- test/test.js
+++ test/test.js
@@ -1,2 +1,30 @@
+const assert = require('assert');
+const axios = require("axios")
 
-const { app }= require('./app')
+const { app } = require('./app');
+const PORT = 3000;
+
+let server;
+
+describe("It should test the function to send an email on catching an error", () => {
+
+    before("Spinning up the servers", () => {
+        server = app.listen(PORT, () => {
+            console.log(`Example app listening on port ${PORT}`);
+        });
+    });
+
+    it("Should test the endpoint with the error", async () => {
+        try {
+            await axios.get("http://localhost:3000/");
+        } catch (error) {
+            assert.equal(error.response.status, 500);
+            assert.equal(error.response.data.message, "Error");
+            assert.equal(error.response.data.mailSent, true);
+        }
+    });
+
+    after("Closing the servers", () => {
+        server.close();
+    });
+});
